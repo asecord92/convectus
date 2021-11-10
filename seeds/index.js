10c0a9c7406b76a672f2db2b099798b82f9723cf
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-11-09 22:34:16 -0800

    added sequelize seed for rsvps

--- seeds/index.js
+++ seeds/index.js
@@ -1,23 +1,22 @@
 const seedEvents = require('./event-seeds');
 const seedUsers = require('./user-seeds');
+const seedRsvps = require('./rsvp-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('--------------');
-//   await seedUsers();
-//   console.log('--------------');
+    await sequelize.sync({ force: true });
+    console.log('--------------');
 
 
-await seedUsers();
-console.log('--------------');
+    await seedUsers();
+    console.log('--------------');
 
-  await seedEvents();
-  console.log('--------------');
+    await seedEvents();
+    console.log('--------------');
 
-//   await seedVotes();
-//   console.log('--------------');
+    await seedRsvps();
+    console.log('--------------');
 
   process.exit(0);
 };
